2909e816779f79bd7b939f59f1abc8cddbafec68
omgohmygod <t105590013@ntut.org.tw>
2018-05-07 23:36:06 +0800

    這是HW7 用HW6直接改的

--- HW6/app/src/main/java/com/example/admin/hw6/GameResultFragment.java
+++ HW6/app/src/main/java/com/example/admin/hw6/GameResultFragment.java
@@ -1,65 +1,56 @@
 package com.example.admin.hw6;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GameResultFragment extends Fragment {
+public class GameResultFragment extends AppCompatActivity {
 
-    private EditText mEdtCountSet,
-            mEdtCountPlayerWin,
-            mEdtCountComWin,
-            mEdtCountDraw;
-
-    public GameResultFragment() {
-        // Required empty public constructor
-    }
-
+    // 宣告有哪些元件
+    private Button btnReturn;
+    private EditText edtCountSet, edtCountPlayerWin, edtCountComWin, edtCountDraw;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_result, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_game_result);
+        // 幫元件找ID
+        btnReturn = (Button) findViewById(R.id.btnreturn);
+        btnReturn.setOnClickListener(btnOnReturn);
+        edtCountSet = (EditText) findViewById(R.id.edtCountSet);
+        edtCountPlayerWin = (EditText) findViewById(R.id.edtCountPlayerWin);
+        edtCountComWin = (EditText) findViewById(R.id.edtCountComWin);
+        edtCountDraw = (EditText) findViewById(R.id.edtCountDraw);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            edtCountSet.setText(String.valueOf(bundle.getInt("miCountSet")));
+            edtCountPlayerWin.setText(String.valueOf(bundle.getInt("miCountPlayerWin")));
+            edtCountComWin.setText(String.valueOf(bundle.getInt("miCountComWin")));
+            edtCountDraw.setText(String.valueOf(bundle.getInt("miCountDraw")));
+        } else {
+            Toast.makeText(this, "資料不能為空！", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        mEdtCountSet = (EditText) getActivity().findViewById(R.id.edtCountSet);
-        mEdtCountPlayerWin = (EditText) getActivity().findViewById(R.id.edtCountPlayerWin);
-        mEdtCountComWin = (EditText) getActivity().findViewById(R.id.edtCountComWin);
-        mEdtCountDraw = (EditText) getActivity().findViewById(R.id.edtCountDraw);
-
-        ((MainActivity) getActivity()).mGameResultType = MainFragment.GameResultType.TYPE_1;
-        ((MainActivity) getActivity()).fragResult = this;
-
-        getActivity().findViewById(R.id.fragment_container).setVisibility(View.VISIBLE);
-        ((MainActivity) getActivity()).InvokeUpdateResult(); // Update values immediately
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().findViewById(R.id.fragment_container).setVisibility(View.GONE);
-    }
-
-    public void updateGameResult(int iCountSet,
-                                 int iCountPlayerWin,
-                                 int iCountComWin,
-                                 int iCountDraw) {
-        mEdtCountSet.setText(String.valueOf(iCountSet));
-        mEdtCountDraw.setText(String.valueOf(iCountDraw));
-        mEdtCountComWin.setText(String.valueOf(iCountComWin));
-        mEdtCountPlayerWin.setText(String.valueOf(iCountPlayerWin));
-    }
+    private View.OnClickListener btnOnReturn = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //返回
+            finish();
+        }
+    };
 }
--- HW6/app/src/main/java/com/example/admin/hw6/MainActivity.java
+++ HW6/app/src/main/java/com/example/admin/hw6/MainActivity.java
@@ -1,81 +1,109 @@
 package com.example.admin.hw6;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.CallbackInterface {
+public class MainActivity extends AppCompatActivity {
 
-    private final static String TAG = "Result";
-    private int mTagCount = 0;
-    public MainFragment.GameResultType mGameResultType;
-    public MainFragment mainFragment;
-    public Fragment fragResult;
+    // 骰子的圖片陣列
+    public int[] diceImg = new int[]{
+            R.drawable.dice01, R.drawable.dice02, R.drawable.dice03,
+            R.drawable.dice04, R.drawable.dice05, R.drawable.dice06
+    };
+    // 宣告有哪些元件
+    private Button btnstart, btnresult;
+    private ImageView mImgDice;
+    // 新增統計遊戲局數和輸贏的變數
+    private int miCountSet = 0, miCountPlayerWin = 0, miCountComWin = 0, miCountDraw = 0;
+    // 本次作業重點
+    private Intent intent = new Intent();
+    private Bundle bundle = new Bundle();
+    // 一點點的防呆 檢查動畫播完沒
+    private Boolean isDiceRolling = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.fragMain);
-
+        // 幫元件找ID
+        btnstart = (Button) findViewById(R.id.btnstart);
+        btnresult = (Button) findViewById(R.id.btnresult);
+        mImgDice = (ImageView) findViewById(R.id.imgDice);
+        // 本次作業重點 設定物件擁有者                     // 要啟動的 Activity class
+        intent.setClass(MainActivity.this, GameResultFragment.class);
+        //  設置按鍵監聽
+        btnstart.setOnClickListener(btnOnclick);
+        btnresult.setOnClickListener(btnOnclick);
+    }
+    //處理輸贏、平手 用 Toast 顯示
+    public void resultDice() {
+        int num = (int) (Math.random() * 6);
+        mImgDice.setImageDrawable(getResources().getDrawable(diceImg[num]));
+        miCountSet += 1;
+        String result;
+        switch (num / 2) {
+            case 0: // win
+                result = getString(R.string.player_win);
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                miCountPlayerWin += 1;
+                break;
+            case 1: // draw
+                result = getString(R.string.player_draw);
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                miCountDraw += 1;
+                break;
+            case 2: // lose
+                result = getString(R.string.player_lose);
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                miCountComWin += 1;
+                break;
+        }
     }
 
-    @Override
-    public void updateGameResult(int iCountSet, int iCountPlayerWin, int iCountComWin, int iCountDraw) {
-        if (findViewById(R.id.fragment_container).isShown()) {
-            switch (mGameResultType) {
-                case TYPE_1:
-                    ((GameResultFragment) fragResult).updateGameResult(iCountSet, iCountPlayerWin,
-                            iCountComWin, iCountDraw);
+    private View.OnClickListener btnOnclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnstart:
+                    if (isDiceRolling) return;
+                    isDiceRolling = true;
+                    //從動畫資源中取得動畫檔，設定給ImageView物件，然後開始播放
+                    Resources res = getResources();
+                    final AnimationDrawable animDraw = (AnimationDrawable) res.getDrawable(R.drawable.anim_roll_dice);
+                    mImgDice.setImageDrawable(animDraw);
+                    animDraw.start(); // 啟動動畫
+                    Handler handler = new Handler(); // 延遲
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            animDraw.stop();
+                            resultDice();
+                            isDiceRolling = false;
+                        }
+                    }, 2000);
+                    break;
+                case R.id.btnresult:
+                    // 先把要送出的 Data 與 key 綁起來，依序存進 bundle 裡，再把 bundle 存入 intent ，準備送出 Intent 物件要求
+                    bundle.putInt("miCountSet",miCountSet);
+                    bundle.putInt("miCountPlayerWin",miCountPlayerWin);
+                    bundle.putInt("miCountComWin",miCountComWin);
+                    bundle.putInt("miCountDraw",miCountDraw);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
                     break;
             }
         }
-
-    }
-// Update values immediately
-    public void InvokeUpdateResult() {
-        mainFragment.UpdateResult();
-    }
-
-    @Override
-    public void enableGameResult(MainFragment.GameResultType type) {
-        FragmentTransaction fragTran;
-        String sFragTag;
-
-        switch (type) {
-            case TYPE_1:
-                GameResultFragment frag = new GameResultFragment();
-                fragTran = getSupportFragmentManager().beginTransaction();
-                mTagCount++;
-                sFragTag = TAG + new Integer(mTagCount).toString();
-                fragTran.replace(R.id.fragment_container, frag, sFragTag);
-                fragTran.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                fragTran.addToBackStack(null);
-                fragTran.commit();
-                break;
-            case TYPE_2:
-
-                fragTran = getSupportFragmentManager().beginTransaction();
-                mTagCount++;
-                sFragTag = TAG + new Integer(mTagCount).toString();
-
-                fragTran.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                fragTran.addToBackStack(null);
-                fragTran.commit();
-                break;
-            case HIDE:
-                FragmentManager fragMgr = getSupportFragmentManager();
-                sFragTag = TAG + new Integer(mTagCount).toString();
-                Fragment fragGameResult = fragMgr.findFragmentByTag(sFragTag);
-                fragTran = fragMgr.beginTransaction();
-                fragTran.remove(fragGameResult);
-                fragTran.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                fragTran.addToBackStack(null);
-                fragTran.commit();
-                break;
-        }
-
-    }
+    };
 }
+
