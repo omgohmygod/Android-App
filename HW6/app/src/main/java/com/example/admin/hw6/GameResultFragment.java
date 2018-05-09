d6eb2b7bab03b6f969cc4ef7b6f13dd5bf26226f
omgohmygod <t105590013@ntut.org.tw>
2018-05-09 23:11:41 +0800

    fix bug

--- HW6/app/src/main/java/com/example/admin/hw6/GameResultFragment.java
+++ HW6/app/src/main/java/com/example/admin/hw6/GameResultFragment.java
@@ -5,9 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,30 +16,29 @@ import android.widget.Toast;
  */
 public class GameResultFragment extends AppCompatActivity {
 
-    // 宣告有哪些元件
+
     private Button btnReturn;
-    private EditText edtCountSet, edtCountPlayerWin, edtCountComWin, edtCountDraw;
+    private EditText countgame, countplayer, countcom, countdraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_game_result);
-        // 幫元件找ID
         btnReturn = (Button) findViewById(R.id.btnreturn);
         btnReturn.setOnClickListener(btnOnReturn);
-        edtCountSet = (EditText) findViewById(R.id.edtCountSet);
-        edtCountPlayerWin = (EditText) findViewById(R.id.edtCountPlayerWin);
-        edtCountComWin = (EditText) findViewById(R.id.edtCountComWin);
-        edtCountDraw = (EditText) findViewById(R.id.edtCountDraw);
+        countgame = (EditText) findViewById(R.id.edtCountSet);
+        countplayer = (EditText) findViewById(R.id.edtCountPlayerWin);
+        countcom = (EditText) findViewById(R.id.edtCountComWin);
+        countdraw = (EditText) findViewById(R.id.edtCountDraw);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         if (bundle != null) {
-            edtCountSet.setText(String.valueOf(bundle.getInt("miCountSet")));
-            edtCountPlayerWin.setText(String.valueOf(bundle.getInt("miCountPlayerWin")));
-            edtCountComWin.setText(String.valueOf(bundle.getInt("miCountComWin")));
-            edtCountDraw.setText(String.valueOf(bundle.getInt("miCountDraw")));
+            countgame.setText(String.valueOf(bundle.getInt("miCountSet")));
+            countplayer.setText(String.valueOf(bundle.getInt("miCountPlayerWin")));
+            countcom.setText(String.valueOf(bundle.getInt("miCountComWin")));
+            countdraw.setText(String.valueOf(bundle.getInt("miCountDraw")));
         } else {
-            Toast.makeText(this, "資料不能為空！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Data is empty！", Toast.LENGTH_SHORT).show();
             finish();
         }
     }
@@ -49,7 +46,6 @@ public class GameResultFragment extends AppCompatActivity {
     private View.OnClickListener btnOnReturn = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //返回
             finish();
         }
     };
--- HW6/app/src/main/java/com/example/admin/hw6/MainActivity.java
+++ HW6/app/src/main/java/com/example/admin/hw6/MainActivity.java
@@ -6,9 +6,6 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,57 +13,50 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 骰子的圖片陣列
     public int[] diceImg = new int[]{
             R.drawable.dice01, R.drawable.dice02, R.drawable.dice03,
             R.drawable.dice04, R.drawable.dice05, R.drawable.dice06
     };
-    // 宣告有哪些元件
+
     private Button btnstart, btnresult;
     private ImageView mImgDice;
-    // 新增統計遊戲局數和輸贏的變數
-    private int miCountSet = 0, miCountPlayerWin = 0, miCountComWin = 0, miCountDraw = 0;
-    // 本次作業重點
+    private int countgame = 0, countplayer = 0, countcom = 0, countdraw = 0;
     private Intent intent = new Intent();
     private Bundle bundle = new Bundle();
-    // 一點點的防呆 檢查動畫播完沒
     private Boolean isDiceRolling = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 幫元件找ID
         btnstart = (Button) findViewById(R.id.btnstart);
         btnresult = (Button) findViewById(R.id.btnresult);
         mImgDice = (ImageView) findViewById(R.id.imgDice);
-        // 本次作業重點 設定物件擁有者                     // 要啟動的 Activity class
         intent.setClass(MainActivity.this, GameResultFragment.class);
-        //  設置按鍵監聽
         btnstart.setOnClickListener(btnOnclick);
         btnresult.setOnClickListener(btnOnclick);
     }
-    //處理輸贏、平手 用 Toast 顯示
-    public void resultDice() {
+
+    public void dice() {
         int num = (int) (Math.random() * 6);
         mImgDice.setImageDrawable(getResources().getDrawable(diceImg[num]));
-        miCountSet += 1;
+        countgame += 1;
         String result;
         switch (num / 2) {
-            case 0: // win
+            case 0:
                 result = getString(R.string.player_win);
                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
-                miCountPlayerWin += 1;
+                countplayer += 1;
                 break;
-            case 1: // draw
+            case 1:
                 result = getString(R.string.player_draw);
                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
-                miCountDraw += 1;
+                countdraw += 1;
                 break;
-            case 2: // lose
+            case 2:
                 result = getString(R.string.player_lose);
                 Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
-                miCountComWin += 1;
+                countcom += 1;
                 break;
         }
     }
@@ -78,27 +68,27 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btnstart:
                     if (isDiceRolling) return;
                     isDiceRolling = true;
-                    //從動畫資源中取得動畫檔，設定給ImageView物件，然後開始播放
+
                     Resources res = getResources();
                     final AnimationDrawable animDraw = (AnimationDrawable) res.getDrawable(R.drawable.anim_roll_dice);
                     mImgDice.setImageDrawable(animDraw);
-                    animDraw.start(); // 啟動動畫
-                    Handler handler = new Handler(); // 延遲
+                    animDraw.start();
+                    Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         @Override
                         public void run() {
                             animDraw.stop();
-                            resultDice();
+                            dice();
                             isDiceRolling = false;
                         }
                     }, 2000);
                     break;
+
                 case R.id.btnresult:
-                    // 先把要送出的 Data 與 key 綁起來，依序存進 bundle 裡，再把 bundle 存入 intent ，準備送出 Intent 物件要求
-                    bundle.putInt("miCountSet",miCountSet);
-                    bundle.putInt("miCountPlayerWin",miCountPlayerWin);
-                    bundle.putInt("miCountComWin",miCountComWin);
-                    bundle.putInt("miCountDraw",miCountDraw);
+                    bundle.putInt("miCountSet",countgame);
+                    bundle.putInt("miCountPlayerWin",countplayer);
+                    bundle.putInt("miCountComWin",countcom);
+                    bundle.putInt("miCountDraw", countdraw);
                     intent.putExtras(bundle);
                     startActivity(intent);
                     break;
